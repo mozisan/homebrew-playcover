61402f605d399ac93b1c55fac61c596e3a11c9d8
('3') <mozisan@users.noreply.github.com>
2026-08-13 14:54:51 +0900

    Fix PlayCover nightly artifact downloads

--- Casks/playcover-nightly.rb
+++ Casks/playcover-nightly.rb
@@ -1,14 +1,23 @@
+class PlayCoverNightlyDownloadStrategy < CurlDownloadStrategy
+  def initialize(url, name, version, **meta)
+    page = ::Utils::Curl.curl_output("--fail", "--location", url).stdout
+    artifact_url = page[/href="([^"]+\.dmg\.zip)"/, 1]
+    raise CurlDownloadStrategyError.new(url, "Nightly artifact URL was not found") if artifact_url.nil?
+
+    super(artifact_url, name, version, **meta)
+  end
+end
+
 cask "playcover-nightly" do
   version :latest
   sha256 :no_check
 
-  url "https://nightly.link/playcover/playcover/workflows/2.nightly_release/develop" do |page|
-    page[/href="([^"]+\.dmg.zip)"/, 1]
-  end
+  url "https://nightly.link/playcover/playcover/workflows/2.nightly_release/develop",
+      using: PlayCoverNightlyDownloadStrategy
   name "PlayCover"
   desc "Sideload iOS apps and games"
   homepage "https://github.com/PlayCover/PlayCover"
-  
+
   auto_updates true
   conflicts_with cask: "playcover-community"
   depends_on arch: :arm64
